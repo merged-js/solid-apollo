2f9eb47afd9c07ef9d1697df62a35ac678960b6c
Torsten <info@by-torsten.com>
2021-10-29 23:32:40 +0200

    feat: check for correct <ApolloProvider /> setup

--- src/ApolloProvider.tsx
+++ src/ApolloProvider.tsx
@@ -12,4 +12,12 @@ export const ApolloProvider: Component<ApolloProviderProps> = props => (
   <ApolloContext.Provider value={props.client}>{props.children}</ApolloContext.Provider>
 )
 
-export const useApollo = () => useContext(ApolloContext)
+export const useApollo = () => {
+  const apolloClient = useContext(ApolloContext)
+  if (!apolloClient) {
+    throw new Error(
+      'apolloClient could not be found in context. Did you wrap your Component with <ApolloProvider client={...} /> ?'
+    )
+  }
+  return apolloClient
+}
